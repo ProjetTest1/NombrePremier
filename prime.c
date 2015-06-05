35081d372564d41a379f202bf2af4deebd9f5b32
Zakaridia <zakaridiadiawara@gmail.com>
2015-06-05 22:49:09 +0200

    Application d'un autre algorithme de test de primalité

--- prime.c
+++ prime.c
@@ -1,5 +1,20 @@
 #include "prime.h"
 
-int isPrime(long number){
-	return  number % 2 == 0; 
+// Permet de dire si un nombre est premier 
+bool isPrime(int number){
+	if(number==0 || number ==1)
+		return false;
+		
+	if( number%2 == 0 ){   // Le nombre est pair
+		return number==2;  //On retourne sa comparaison avec 2
+	}
+	else{
+		int m = number/2;
+		int i;
+		for( i = 3; i < m; i++){   // On sarrete a la racine carree su nombre
+			if(number%i == 0)   // On a trouvé un diviseur
+				return false;
+		}
+		return true;  // Pas de diviseur donc le nombre est premier 
+	}
 }
