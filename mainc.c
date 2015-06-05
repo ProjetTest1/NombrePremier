c042c3c110455eacebe4fbcdc702962d31215c8b
Zakaridia <zakaridiadiawara@gmail.com>
2015-06-05 22:51:13 +0200

    Test de la fonction isPrime avec possibilé d'envoyer un nombre en parametre

--- mainc.c
+++ mainc.c
@@ -1,6 +1,22 @@
 #include "prime.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(){
-	isPrime(8);
+int main(int argc , char** argv){
+	int a;
+	if( argc != 1 )
+		a = atoi(argv[1]);
+	else{
+		printf("Saisir un nombre ");
+		scanf("%d",&a);
+	}
+	if( isPrime(a) ) {
+		printf("%d est premier\n",a);
+	}
+	else{
+		printf("%d n'est pas premier\n",a);
+	}
+
 	return 0;
+
 }
